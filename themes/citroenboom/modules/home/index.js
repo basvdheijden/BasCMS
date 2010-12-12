97b6ed340eb731d41aece7b491107bad14c744e4
BasVanDerHeijden <citroenbomen@gmail.com>
2010-12-12 16:13:18 +0100

    formatter added for displaying multiple pieces of content

--- themes/citroenboom/modules/home/index.js
+++ themes/citroenboom/modules/home/index.js
@@ -1,7 +1,26 @@
 exports.init = function(app) {
+	var home = {
+		pages: {
+			title: 'Home',
+			body: 'Ik ben thuis',
+		}
+	};
+
 	app.get('/', function(req, res) {
-		exports.modules['menus'].render_menu(['primary'], function(items){
-			exports.modules['themer'].render(req, res, 'menu', items);	
-		});
+		exports.format(req, res, home);
 	});
 }
+
+exports.format = function(req, res, data, callback) {
+	exports.modules['menus'].render_menu(['primary'], function(items){
+		var page = {
+			menu: items,
+		};
+
+		for(var i in data) {
+			page[i] = data[i];	
+		}
+
+		exports.modules['themer'].render(req, res, '', page);	
+	});
+};
